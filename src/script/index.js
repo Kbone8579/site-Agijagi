409303d77f27bdb962551f4c0d7e70211c542cfb
kbone8579 <a01056718579@gmail.com>
2024-12-14 13:14:17 +0900

    work

--- src/script/index.js
+++ src/script/index.js
@@ -42,15 +42,26 @@ window.onload = function(){
     });
 
     // 클릭시 해당 페이지로 이동
-    document.querySelector(".btn1").addEventListener("click", (e) => {
-      document.querySelector(".fage.s1").scrollIntoView({ behavior: "smooth"});
-    });
-    document.querySelector(".btn2").addEventListener("click", (e) => {
+    // 페이지1
+    document.querySelector(".number-btn.s1 .btn2").addEventListener("click", (e) => {
       document.querySelector(".fage.s2").scrollIntoView({ behavior: "smooth"});
     });
-    document.querySelector(".btn3").addEventListener("click", (e) => {
+    document.querySelector(".number-btn.s1 .btn3").addEventListener("click", (e) => {
       document.querySelector(".fage.s3").scrollIntoView({ behavior: "smooth"});
     });
-    
+    // 페이지2
+    document.querySelector(".number-btn.s2 .btn1").addEventListener("click", (e) => {
+      document.querySelector(".fage.s1").scrollIntoView({ behavior: "smooth"});
+    });
+    document.querySelector(".number-btn.s2 .btn3").addEventListener("click", (e) => {
+      document.querySelector(".fage.s3").scrollIntoView({ behavior: "smooth"});
+    });
+    // 페이지3
+    document.querySelector(".number-btn.s3 .btn1").addEventListener("click", (e) => {
+      document.querySelector(".fage.s1").scrollIntoView({ behavior: "smooth"});
+    });
+    document.querySelector(".number-btn.s3 .btn2").addEventListener("click", (e) => {
+      document.querySelector(".fage.s2").scrollIntoView({ behavior: "smooth"});
+    });
   };
 
