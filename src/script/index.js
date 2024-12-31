a0fde1acd185fabbdb66c08677ba741f0bb1ca58
kbone8579 <a01056718579@gmail.com>
2024-12-31 18:46:55 +0900

    work

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,4 @@
-window.onload = function(){
-
+window.addEventListener('DOMContentLoaded', function(){
     // 한 페이지씩 이동
     const elm = document.querySelectorAll('.fage');
     const elmCount = elm.length;
@@ -63,34 +62,27 @@ window.onload = function(){
     document.querySelector(".number-btn.s3 .btn2").addEventListener("click", (e) => {
       document.querySelector(".fage.s2").scrollIntoView({ behavior: "smooth"});
     });
-  };
-
-  // section4 클릭 애니메이션
-  // $(document).ready(function(){
-  //   // 기본 페이지 안 보이게 하기
-  //     $(".fage.s2").fadeOut(500);
-  //     $(".fage.s3").fadeOut(500);
-  //     $(".fage.s4").fadeOut(500);
-    
-  //    // 클릭시 해당 지속가능성 들어가기 
-  //   $(".img-item.s1").click(function(){
-  //     $(".fage.s1").fadeOut(500);
-  //     $(".fage.s2").fadeIn(500);
-  //   });
-  //   $(".img-item.s2").click(function(){
-  //     $(".fage.s1").fadeOut(500);
-  //     $(".fage.s3").fadeIn(500);
-  //   });
-  //   $(".img-item.s3").click(function(){
-  //     $(".fage.s1").fadeOut(500);
-  //     $(".fage.s4").fadeIn(500);
-  //   });
-  //   // 뒤로가기
-  //   $(".back-btn-2").click(function(){
-  //     $(".fage.s1").fadeIn(500);
-  //     $(".fage.s2").fadeOut(500);
-  //     $(".fage.s3").fadeOut(500);
-  //     $(".fage.s4").fadeOut(500);
-  //   });
-  // });
+    // 클릭시 해당 페이지로 이동
+    // 페이지5
+    document.querySelector(".number-btn.s4 .btn2").addEventListener("click", (e) => {
+      document.querySelector(".fage.s6").scrollIntoView({ behavior: "smooth"});
+    });
+    document.querySelector(".number-btn.s4 .btn3").addEventListener("click", (e) => {
+      document.querySelector(".fage.s7").scrollIntoView({ behavior: "smooth"});
+    });
+    // 페이지6
+    document.querySelector(".number-btn.s5 .btn1").addEventListener("click", (e) => {
+      document.querySelector(".fage.s5").scrollIntoView({ behavior: "smooth"});
+    });
+    document.querySelector(".number-btn.s5 .btn3").addEventListener("click", (e) => {
+      document.querySelector(".fage.s7").scrollIntoView({ behavior: "smooth"});
+    });
+    // 페이지7
+    document.querySelector(".number-btn.s6 .btn1").addEventListener("click", (e) => {
+      document.querySelector(".fage.s5").scrollIntoView({ behavior: "smooth"});
+    });
+    document.querySelector(".number-btn.s6 .btn2").addEventListener("click", (e) => {
+      document.querySelector(".fage.s6").scrollIntoView({ behavior: "smooth"});
+    });
+});
 
