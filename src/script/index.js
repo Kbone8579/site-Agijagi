8f3c054e273b6d16328a13cd03326c11c3ed0241
kbone8579 <a01056718579@gmail.com>
2025-01-09 17:29:21 +0900

    work

--- src/script/index.js
+++ src/script/index.js
@@ -256,3 +256,35 @@ $(document).ready(function () {
     showItems("s8");
   });
 });
+
+
+// 스와이퍼
+$(document).ready(function(){
+  const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
+  var swiper = new Swiper(".swiper-box-1 .swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    direction: "vertical",
+    effect:'fade',
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      bulletActiveClass: 'on',
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        // progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    }
+  });
+});
